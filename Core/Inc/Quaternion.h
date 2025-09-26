0d3c1f9e1354a436ec45c3ec0ea1abfbfec7b8d0
rrr-jh <dlwoguddd@gmail.com>
2025-09-26 20:47:37 +0900

    Quaternion update

--- Core/Inc/Quaternion.h
+++ Core/Inc/Quaternion.h
@@ -1,13 +1,14 @@
-/*
- * Quaternion.h
- *
- *  Created on: Sep 21, 2025
- *      Author: jh
- */
+#ifndef _QUATERNION_H
+#define _QUATERNION_H
 
-#ifndef INC_QUATERNION_H_
-#define INC_QUATERNION_H_
+#include "main.h"
+#include <math.h>
 
+extern float BNO080_Roll;
+extern float BNO080_Pitch;
+extern float BNO080_Yaw;
 
+void Quaternion_Update(float* q);
+float invSqrt(float x);
 
-#endif /* INC_QUATERNION_H_ */
+#endif
